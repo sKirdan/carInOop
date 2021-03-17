137136f36ddecc63f05e9d5b800524943a04f367
sKirdan <rex1532007@gmail.com>
2021-03-17 21:46:37 +0100

    carCrashed

--- js/app.js
+++ js/app.js
@@ -1,5 +1,20 @@
 function carStartListener(){
-    alert('button clicked')
+    var randomNamber = Math.random()
+
+    if (randomNamber > 0.5){
+        alert('car have started')
+
+        startButton.classList.add('hide')
+
+        function engineCrashed(){
+            startButton.classList.remove('hide')
+            alert('engine crashed')
+        }
+        window.setTimeout(engineCrashed, 5000)
+        alert('we wait crash')
+    }else{
+        alert('something wrong')
+    }
 }
 
 var startButton = document.querySelector('#start-car')
