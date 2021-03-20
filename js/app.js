c8f0ea8acab3e7c231753f9eaf2303f975e8f349
sKirdan <rex1532007@gmail.com>
2021-03-20 18:18:40 +0100

    +GearBox

--- js/app.js
+++ js/app.js
@@ -1,38 +1,57 @@
-function carStartListener(){
+function carStartListener() {
     var randomNamber = Math.random()
 
-    if (randomNamber > 0.5){
+    if (randomNamber > 0.5) {
         carStarted()
-    }else{
+    } else {
         carCannotBeStarted()
     }
 }
-function drawLabel(status){
+function drawLabel(status) {
     statusLable.innerHTML = status;
-    
+
 }
-function devLog(messege){
+function devLog(messege) {
     console.log(messege)
 }
-function carStarted(){
+function carStarted() {
     drawLabel('car have started')
     devLog('car have started')
 
     startButton.classList.add('hide')
 
-    function engineCrashed(){
-        startButton.classList.remove('hide')
-        devLog('engine crashed')
-        drawLabel('Engine have crashed. Car stopped')
-    }
-    window.setTimeout(engineCrashed, 2000)
+    gearBoxStarted()
+
+    plannedCrashStarted()
     devLog('we wait crash')
 }
-function carCannotBeStarted(){
+function carCannotBeStarted() {
     devLog('something wrong')
     drawLabel('Car cant be started. Try again')
 }
-
+function gearBoxStarted() {
+    var gearBoxValue = 1
+    gearBoxValueLable.innerHTML = gearBoxValue
+    function incrementGearBoxValue() {
+        if (gearBoxValue < 5) {
+            gearBoxValue++
+            gearBoxValueLable.innerHTML = gearBoxValue
+        }
+    }
+    gearBoxInterval = window.setInterval(incrementGearBoxValue, 1000)
+}
+function plannedCrashStarted() {
+    function engineCrashed() {
+        startButton.classList.remove('hide')
+        devLog('engine crashed')
+        drawLabel('Engine have crashed. Car stopped')
+        gearBoxValueLable.innerHTML = 'N'
+        window.clearInterval(gearBoxInterval)
+    }
+    window.setTimeout(engineCrashed, 3000)
+}
+var gearBoxInterval
 var startButton = document.querySelector('#start-car')
 var statusLable = document.querySelector('#status')
+var gearBoxValueLable = document.querySelector('#gear-box-value')
 startButton.addEventListener('click', carStartListener)
