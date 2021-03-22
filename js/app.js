87f5fb7266fb22ca4c038296c8f2ac9643b1915e
sKirdan <rex1532007@gmail.com>
2021-03-22 12:15:51 +0100

    debugQuetySelectorAll

--- js/app.js
+++ js/app.js
@@ -7,17 +7,18 @@ function carStartListener() {
         carCannotBeStarted()
     }
 }
+
 function drawStatus(status) {
     processEls(statusLabels, function (statusLabel) {
         statusLabel.innerHTML = status
     })
 }
 
-
-
 function devLog(messege) {
     console.log(messege)
+
 }
+
 function carStarted() {
     drawStatus('car have started')
     devLog('car have started')
@@ -26,22 +27,25 @@ function carStarted() {
         startButton.classList.add("hide")
     })
 
-
     gearBoxStarted()
 
     plannedCrashStarted()
+
     devLog('we wait crash')
 }
+
 function processEls(arrayOfEls, proccessor) {
     for (var i = 0; i < arrayOfEls.length; i++) {
         var item = arrayOfEls[i];
         proccessor(item)
     }
 }
+
 function carCannotBeStarted() {
     devLog('something wrong')
     drawStatus('Car cant be started. Try again')
 }
+
 function gearBoxStarted() {
     var gearBoxValue = 1
     processEls(gearBoxValueLabels, function (gearBoxValueLabel) {
@@ -59,6 +63,7 @@ function gearBoxStarted() {
     }
     gearBoxInterval = window.setInterval(incrementGearBoxValue, 1000)
 }
+
 function plannedCrashStarted() {
     function engineCrashed() {
         devLog('engine crashed')
@@ -75,10 +80,11 @@ function plannedCrashStarted() {
     }
     window.setTimeout(engineCrashed, 3000)
 }
+
 var gearBoxInterval
-var startButtons = document.querySelector("[data-role ='start-car']")
-var statusLabels = document.querySelector("[data-role = 'status']")
-var gearBoxValueLabels = document.querySelector("[data-role ='gear-box-value']")
+var startButtons = document.querySelectorAll("[data-role ='start-car']")
+var statusLabels = document.querySelectorAll("[data-role = 'status']")
+var gearBoxValueLabels = document.querySelectorAll("[data-role ='gear-box-value']")
 
 processEls(startButtons, function (startButton) {
     startButton.addEventListener("click", carStartListener)
